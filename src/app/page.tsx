723d4541a80c6c7f7c3bc2c103202dc6ef7331d1
HitsujiRere <hitsujirere@gmail.com>
2025-08-18 21:42:43 +0900

    feat: HeroUIの追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+"use client";
+
+import { Button } from "@heroui/react";
+
 export default function Home() {
   return (
     <main>
       <p>Hello world!</p>
+      <Button>Click me!</Button>
     </main>
   );
 }
