3c44074f5f45fa37183c7c9c85d02dbed60f3420
HitsujiRere <hitsujirere@gmail.com>
2025-08-23 19:15:23 +0900

    feat: チームメンバー決定ボタンの追加

--- src/features/RoomTable/index.tsx
+++ src/features/RoomTable/index.tsx
@@ -4,6 +4,7 @@ import { UsersIcon } from "lucide-react";
 import React from "react";
 import { useShallow } from "zustand/shallow";
 import { useRoomSummonersStore } from "@/app/stores/useRoomSummonersStore";
+import { Button } from "@/components/ui/button";
 import {
   Table,
   TableBody,
@@ -23,12 +24,27 @@ export const RoomTable = () => {
     setSelectionNames(roomNames);
   }, [roomNames, setSelectionNames]);
 
+  const selectedNames = useSelectionStores(
+    useShallow((state) => state.selectedNames()),
+  );
+
   return (
     <>
       <h2 className="mb-2 inline-flex items-center gap-2 text-xl">
         <UsersIcon className="size-5" />
         ルームサモナー
       </h2>
+
+      <div className="mb-2">
+        <Button
+          disabled={selectedNames.length !== 10}
+          onClick={() => {
+            console.log(selectedNames);
+          }}
+        >
+          チームメンバー決定 {selectedNames.length}/10人
+        </Button>
+      </div>
 
       <div>
         <div className="overflow-hidden rounded-md border">
